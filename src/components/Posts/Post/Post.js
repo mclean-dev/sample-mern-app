d5ec94d52d6b678685567fe50b5e9f3d2bd7288b
John McLean <mclean.jb@gmail.com>
2022-04-17 17:21:27 -0400

    Add: Post update functionality.

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -7,7 +7,7 @@ import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
 import moment from 'moment'
 
 
-const Post = ({ post }) => {
+const Post = ({ post, setCurrentId }) => {
     const classes = useStyles();
 
     return (
@@ -19,15 +19,17 @@ const Post = ({ post }) => {
 
             </div>
             <div className={classes.overlay2}>
-                <Button style={{ color: "white" }} size="small" onClick={() => { }}>
+                <Button style={{ color: "white" }} size="small" onClick={() => setCurrentId(post._id)}>
                     <MoreHorizIcon fontSize="medium" />
                 </Button>
             </div>
             <div className={classes.details}>
                 <Typography variant="body2" color="textSecondary" >{post.tags.map((tag) => `#${tag}`)}</Typography>
             </div>
+            <Typography variant="h5" className={classes.title} gutterBottom>{post.title}</Typography>
+
             <CardContent>
-                <Typography variant="h5" className={classes.title} gutterBottom>{post.message}</Typography>
+                <Typography variant="h5"  gutterBottom>{post.message}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
                 <Button size="small" color="primary" onClick={() => {}}>
