f4ea2375b4e035c1ffcfb8c73a12df05dc5696b6
John McLean <mclean.jb@gmail.com>
2022-04-22 18:02:17 -0400

    Add: middleware

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 
 import postRoutes from './routes/posts.js'
+import userRoutes from './routes/users.js'
 import process from 'process'
 import dotenv from 'dotenv'
 
@@ -15,6 +16,7 @@ app.use(bodyParser.json({limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true }))
 app.use(cors())
 app.use('/posts', postRoutes)
+app.use('/user', userRoutes)
 
 
 const CONNECTION_URL = process.env.CONNECTION_URL;
